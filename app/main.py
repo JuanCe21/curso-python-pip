79b8c64a564937dd90810dff0d0a1d9a2c0a9373
JuanCe <juuanc2@gmail.com>
2023-03-16 17:49:40 -0500

    date set csv

--- app/main.py
+++ app/main.py
@@ -4,23 +4,24 @@ import charts
 
 def run():
   #lectura de data desde enlace
-  data = read_csv.read_csv('./app/data.csv')
+  data = read_csv.read_csv('data.csv')
   #filtro con lambda para solo utilizar la columna continent
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
   #mapeo de la columna country/territory 
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
-  '''
+  
   country = input('Type Country => ')
-
+  print(country)
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
     country = result[0]
+    print(country)
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
-  '''
+    charts.generate_bar_chart(country['Country/Territory'], labels, values)
+  
 
 if __name__ == '__main__':
   run()
