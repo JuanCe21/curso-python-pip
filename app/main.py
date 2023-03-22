55c3451d9346eaf3658dbb8b0047c9ade6b25410
JuanCe <juuanc2@gmail.com>
2023-03-21 21:39:29 -0500

    corrections pandas app

--- app/main.py
+++ app/main.py
@@ -1,19 +1,31 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  #lectura de data desde enlace
-  data = read_csv.read_csv('data.csv')
+  '''
+  
   #filtro con lambda para solo utilizar la columna continent
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
   #mapeo de la columna country/territory 
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
   
+
+  '''
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+
+  #lectura de data desde enlace
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   print(country)
+
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
